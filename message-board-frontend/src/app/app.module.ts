e6b47b4274a97a7a394c8922359fc3c709856d13
Elvan H <elvanhd@gmail.com>
2022-01-22 22:34:25 +0700

    adding the first component

--- message-board-frontend/src/app/app.module.ts
+++ message-board-frontend/src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { PostFormComponent } from './posts/post-form/post-form.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, PostFormComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
