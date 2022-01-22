264ec19a16c9cf5de5bfb383bbf57fe58214a91a
Elvan H <elvanhd@gmail.com>
2022-01-22 23:12:57 +0700

    installing angular material

--- message-board-frontend/src/app/app.module.ts
+++ message-board-frontend/src/app/app.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { PostFormComponent } from './posts/post-form/post-form.component';
 
 @NgModule({
   declarations: [AppComponent, PostFormComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
