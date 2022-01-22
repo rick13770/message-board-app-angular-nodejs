6f72dd3fb1d26e1ecb36677e962cd190d2e36701
Elvan H <elvanhd@gmail.com>
2022-01-22 23:19:15 +0700

    adding a toolbar

--- message-board-frontend/src/app/app.module.ts
+++ message-board-frontend/src/app/app.module.ts
@@ -3,13 +3,15 @@ import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 import { PostFormComponent } from './posts/post-form/post-form.component';
 
 @NgModule({
-  declarations: [AppComponent, PostFormComponent],
+  declarations: [AppComponent, PostFormComponent, HeaderComponent],
   imports: [
     BrowserModule,
     FormsModule,
@@ -17,6 +19,7 @@ import { PostFormComponent } from './posts/post-form/post-form.component';
     MatButtonModule,
     MatCardModule,
     MatInputModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
