fd24e9058a661351d453b41ff3be15d1e25fbd90
Elvan H <elvanhd@gmail.com>
2022-01-23 08:54:05 +0700

    improving the server code

--- message-board-backend/app.js
+++ message-board-backend/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.use((req, res, next) => {
-  console.log('First middleware');
+  // console.log('First middleware');
   next();
 });
 
