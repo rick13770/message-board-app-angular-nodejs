4a71927ec0ce80c6621bedfcb36412a04e39de5d
Elvan H <elvanhd@gmail.com>
2022-01-24 01:25:21 +0700

    adding routing

---
+++ message-board-frontend/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PostFormComponent } from './posts/post-form/post-form.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: PostListComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'create-post',
+    component: PostFormComponent,
+    pathMatch: 'full',
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
--- message-board-frontend/src/app/app.module.ts
+++ message-board-frontend/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { PostFormComponent } from './posts/post-form/post-form.component';
@@ -16,20 +17,21 @@ import { PostListComponent } from './posts/post-list/post-list.component';
 @NgModule({
   declarations: [
     AppComponent,
-    PostFormComponent,
     HeaderComponent,
+    PostFormComponent,
     PostListComponent,
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
     FormsModule,
-    BrowserAnimationsModule,
     HttpClientModule,
     MatButtonModule,
     MatCardModule,
     MatExpansionModule,
     MatInputModule,
     MatToolbarModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
