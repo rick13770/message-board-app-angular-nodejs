1407aa9a872b31db4a69ba5014ebf81e326ac278
Elvan H <elvanhd@gmail.com>
2022-04-06 13:56:42 +0700

    adding route guards

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
+import { GuestGuard } from './auth/guest.guard';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { PostFormComponent } from './posts/post-form/post-form.component';
@@ -15,26 +17,31 @@ const routes: Routes = [
     path: 'create-post',
     component: PostFormComponent,
     pathMatch: 'full',
+    canActivate: [AuthGuard],
   },
   {
     path: 'edit-post/:postId',
     component: PostFormComponent,
     pathMatch: 'full',
+    canActivate: [AuthGuard],
   },
   {
     path: 'login',
     component: LoginComponent,
     pathMatch: 'full',
+    canActivate: [GuestGuard],
   },
   {
     path: 'register',
     component: RegisterComponent,
     pathMatch: 'full',
+    canActivate: [GuestGuard],
   },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
+  providers: [AuthGuard, GuestGuard],
 })
 export class AppRoutingModule {}
---
+++ frontend/src/app/auth/guest.guard.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+
+@Injectable()
+export class GuestGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | boolean
+    | UrlTree
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree> {
+    const isAuthenticated = this.authService.getAuthenticated();
+
+    if (isAuthenticated) {
+      this.router.navigateByUrl('/');
+    }
+
+    return !isAuthenticated;
+  }
+}
